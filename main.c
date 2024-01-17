3d2b4162184080b8ce25ba67232325c96bae5821
Luke Walter <lwalter5667@gmail.com>
2024-01-17 14:32:00 -0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,20 @@
+#include "print.h"
+
+void initialize();
+
+int main() {
+
+    initialize();
+
+    while (1) {
+        
+    }
+
+}
+
+void initialize() {
+
+    short* ptr = 0x05000000;
+    *ptr = 0xFFFF;
+
+}
